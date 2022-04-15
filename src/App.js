587ee7ac477cbdb4e6d150c91f1dbaffe722f0a9
Kles <klesta.luli@gmail.com>
2022-04-15 18:35:38 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react"
+import "./style.css"
+
+export default function App() {
+    return (
+        <div className="App">
+            <h1>Hello from App</h1>
+        </div>
+    )
+}
